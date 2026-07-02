ed5068d01f485af6c39fc8ce548fe5d4812a599f
yangnf-art <yangnf23@gmail.com>
2026-07-02 03:25:53 -0300

    ATT M ADMIN

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import dadosIniciais from './datasMock.json';
+import dadosIniciais from './Datasmock.json';
 
 export default function Admin() {
   // Estados da Agenda Semanal (Dias)
